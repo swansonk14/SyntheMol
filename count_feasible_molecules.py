ad959838001005705a94274b6843ca01524984fd
Kyle Swanson <swansonk.14@gmail.com>
2022-04-23 15:56:16 -0700

    Script to count feasible molecules

---
+++ count_feasible_molecules.py
@@ -0,0 +1,48 @@
+"""Counts the number of feasible molecules using reaction SMARTS from the top REAL reactions."""
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from tap import Tap
+from tqdm import tqdm
+
+from real_reactions import REAL_REACTIONS
+
+
+class Args(Tap):
+    data_path: Path  # Path to CSV file containing building blocks.
+    smiles_column: str = 'smiles'  # Name of the column in molecule_path containing SMILES.
+
+
+def count_feasible_reactions(args: Args) -> None:
+    """Counts the number of feasible molecules using reaction SMARTS from the top REAL reactions."""
+    # Load data
+    building_block_data = pd.read_csv(args.data_path)
+
+    # Convert SMILES to mols
+    building_block_mols = [
+        Chem.AddHs(Chem.MolFromSmiles(smiles))
+        for smiles in tqdm(building_block_data[args.smiles_column], desc='SMILES to mol')
+    ]
+
+    # Process data
+    total_num_molecules = 0
+    for index, reaction in enumerate(tqdm(REAL_REACTIONS, desc='Looping reactions')):
+        reagents = [
+            [
+                building_block_mol
+                for building_block_mol in tqdm(building_block_mols, desc='Matching reagents', leave=False)
+                if building_block_mol.HasSubstructMatch(reagent)
+            ]
+            for reagent in tqdm(reaction['reagents'], desc='Looping reagents', leave=False)
+        ]
+        num_molecules = reaction['counting_fn'](*[len(reagent) for reagent in reagents])
+        print(f'Reaction {index} can produce {num_molecules:,} molecules')
+        total_num_molecules += num_molecules
+
+    print(f'All reactions can produce {total_num_molecules:,} molecules')
+
+
+if __name__ == '__main__':
+    count_feasible_reactions(Args().parse_args())
--- real_reactions.py
+++ real_reactions.py
@@ -1,8 +1,11 @@
 """SMARTS representations of the REAL reactions."""
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from scipy.special import comb
+
 """
 from collections import deque
 
-from rdkit import Chem
 
 class CarbonChainChecker:
     def __init__(self, start_atoms: set[int], end_atoms: set[int]) -> None:
@@ -38,6 +41,27 @@ class CarbonChainChecker:
         return False  # There is no path from any start index to any end index that is all carbon
 """
 
+
+def count_two_different_reagents(num_r1: int, num_r2: int) -> int:
+    """Counts the number of feasible molecules created from two different reagents."""
+    return num_r1 * num_r2
+
+
+def count_two_same_reagents(num_r1: int, num_r2: int) -> int:
+    """Counts the number of feasible molecules created from two of the same reagent."""
+    assert num_r1 == num_r2
+
+    return comb(num_r1, 2, exact=True, repetition=True)
+
+
+def count_three_reagents_with_two_same(num_r1: int, num_r2: int, num_r3: int) -> int:
+    """Counts the number of feasible molecules created from three reagents
+       with the last two the same and the first different."""
+    assert num_r2 == num_r3
+
+    return num_r1 * comb(num_r2, 2, exact=True, repetition=True)
+
+
 # TODO: requires Chem.AddHs()
 REAL_REACTIONS = [
     {
@@ -52,7 +76,8 @@ REAL_REACTIONS = [
         # 'checkers': {  # TODO: maybe check explicitly for carbon chains?
         #     reagent_with_carbon_chain: CarbonChainChecker(start_atoms={8, 9}, end_atoms={10, 13})
         # },
-        'real_ids': {275592}
+        'real_ids': {275592},
+        'counting_fn': count_three_reagents_with_two_same
     },
     {
         'reagents': [
@@ -60,7 +85,8 @@ REAL_REACTIONS = [
             '[OH1][C:4]([*:5])=[O:6]'
         ],
         'product': '[*:5][C:4](=[O:6])[N:2]([*:1])[*:3]',
-        'real_ids': {11, 22, 527, 188690, 240690, 269946, 270006, 270112, 272270, 272430, 273450, 273652, 280130}
+        'real_ids': {11, 22, 527, 188690, 240690, 269946, 270006, 270112, 272270, 272430, 273450, 273652, 280130},
+        'counting_fn': count_two_different_reagents
     },
     {
         'reagents': [
@@ -68,7 +94,8 @@ REAL_REACTIONS = [
             '[*:4][NH1:5][*:6]'
         ],
         'product': 'O=C([N:2]([*:1])[*:3])[N:5]([*:4])[*:6]',
-        'real_ids': {512, 2430, 2554, 2708, 272164, 273390, 273392, 273452, 273454, 273458, 273464, 273574}
+        'real_ids': {512, 2430, 2554, 2708, 272164, 273390, 273392, 273452, 273454, 273458, 273464, 273574},
+        'counting_fn': count_two_same_reagents
     },
     {
         'reagents': [
@@ -76,7 +103,8 @@ REAL_REACTIONS = [
             '[F,Cl,Br,I:4][*:5]'
         ],
         'product': '[*:1][N:2]([*:2])[*:3]',
-        'real_ids': {7, 27, 34, 38, 44, 61, 2230, 269956, 269982, 270122, 270166, 270344, 272692, 272710, 273654}
+        'real_ids': {7, 27, 34, 38, 44, 61, 2230, 269956, 269982, 270122, 270166, 270344, 272692, 272710, 273654},
+        'counting_fn': count_two_different_reagents
     },
     {
         'reagents': [
@@ -84,7 +112,8 @@ REAL_REACTIONS = [
             '[F,Cl,Br,I:3][*:4]'
         ],
         'product': '[*:1][O,S:2][*:4]',
-        'real_ids': {7, 34, 272692, 272710, 273654}
+        'real_ids': {7, 34, 272692, 272710, 273654},
+        'counting_fn': count_two_different_reagents
     },
     {
         'reagents': [
@@ -92,7 +121,8 @@ REAL_REACTIONS = [
             '[*:4][NH1:5][*:6]',
         ],
         'product': 'O=C(C(=O)[N:2]([*:1])[*:3])[N:5]([*:4])[*:6]',
-        'real_ids': {2718, 271948, 271949, 273460, 273462, 273492, 273494, 273496, 273498}
+        'real_ids': {2718, 271948, 271949, 273460, 273462, 273492, 273494, 273496, 273498},
+        'counting_fn': count_two_same_reagents
     },
     {
         'reagents': [
@@ -100,15 +130,17 @@ REAL_REACTIONS = [
             '[O:4]=[S:5](=[O:6])([F,Cl,Br,I:7])[*:8]'
         ],
         'product': '[O:4]=[S:5](=[O:6])([*:8])[N:2]([*:1])[*:3]',
-        'real_ids': {20, 40, 196680, 232682, 270084, 270188, 271082, 273578, 274078}
+        'real_ids': {20, 40, 196680, 232682, 270084, 270188, 271082, 273578, 274078},
+        'counting_fn': count_two_different_reagents
     },
     {
         'reagents': [
-            '[O:1]=[C:2]([O:3])[*:4]'
+            '[O:1]=[C:2]([O:3])[*:4]',
             '[F,Cl,Br,I:5][*:6]'
         ],
         'product': '[O:1]=[C:2]([*:4])[O:3][*:6]',
-        'real_ids': {1458}
+        'real_ids': {1458},
+        'counting_fn': count_two_different_reagents
     }
 ]
 
@@ -116,4 +148,8 @@ REAL_REACTIONS = [
 for reaction in REAL_REACTIONS:
     # TODO: do we need parentheses for the reagents to force them to be separate molecules?
     # reaction = reagent_1.reagent_2...reagent_n>>product
-    reaction['reaction'] = f'{".".join(reaction["reagent"])}>>{reaction["product"]}'
+    reaction['reaction'] = AllChem.ReactionFromSmarts(f'{".".join(reaction["reagents"])}>>{reaction["product"]}')
+
+    # Convert strings to SMARTS objects
+    reaction['reagents'] = [Chem.MolFromSmarts(smarts) for smarts in reaction['reagents']]
+    reaction['product'] = Chem.MolFromSmarts(reaction['product'])
