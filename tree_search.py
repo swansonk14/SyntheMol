f1001950eea08db812f3c2c91d0b6cf1ed456ac8
Kyle Swanson <swansonk.14@gmail.com>
2022-06-01 15:18:59 +0100

    Implemented node expansion

--- tree_search.py
+++ tree_search.py
@@ -1,8 +1,8 @@
 """Contains classes and functions for performing a tree search to generate molecules combinatorially."""
+import itertools
 import math
 from functools import partial
-from itertools import product
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, Optional
 
 import numpy as np
 from rdkit import Chem
@@ -10,7 +10,6 @@ from rdkit import Chem
 from real_reactions import convert_to_mol, Reaction, REAL_REACTIONS
 
 
-# TODO: construction logs
 class TreeNode:
     """A node in a tree search representing a step in the molecule construction process."""
 
@@ -18,7 +17,7 @@ class TreeNode:
                  c_puct: float,
                  scoring_fn: Callable[[str], float],
                  fragments: Optional[tuple[str]] = None,
-                 construction_log: Optional[dict[str, Any]] = None) -> None:
+                 construction_log: Optional[tuple[dict[str, Any]]] = None) -> None:
         """Initializes the TreeNode object.
 
         :param c_puct: The hyperparameter that encourages exploration.
@@ -26,17 +25,17 @@ class TreeNode:
         :param fragments: A tuple of SMILES containing the fragments for the next reaction.
                          The first element is the currently constructed molecule while the remaining elements
                          are the fragments that are about to be added.
-        :param construction_log: A list of dictionaries containing information about each reaction.
+        :param construction_log: A tuple of dictionaries containing information about each reaction.
         """
         self.c_puct = c_puct
         self.scoring_fn = scoring_fn
-        self.fragments = fragments or tuple()
-        self.construction_log = construction_log or []
+        self.fragments = fragments if fragments is not None else tuple()
+        self.construction_log = construction_log if construction_log is not None else tuple()
         # TODO: maybe change sum (really mean) to max since we don't care about finding the best leaf node, just the best node along the way?
         self.W = 0.0  # The sum of the leaf node values for leaf nodes that descend from this node.
         self.N = 0  # The number of leaf nodes that have been visited from this node.
-        self.P = self.compute_score(fragments=fragments, scoring_fn=scoring_fn)  # The property score of this node.
-        self.children: List[TreeNode] = []
+        self._P = None  # The property score of this node. (Computed lazily.)
+        self.children: set[TreeNode] = set()
 
     @classmethod
     def compute_score(cls, fragments: tuple[str], scoring_fn: Callable[[str], float]) -> float:
@@ -50,6 +49,14 @@ class TreeNode:
         """
         # TODO: change this!!! to weight the fragments differently
         return sum(scoring_fn(fragment) for fragment in fragments)
+
+    @property
+    def P(self) -> float:
+        """The property score of this node."""
+        if self._P is None:
+            self._P = self.compute_score(fragments=self.fragments, scoring_fn=self.scoring_fn)
+
+        return self._P
 
     def Q(self) -> float:
         """Value that encourages exploitation of nodes with high reward."""
@@ -75,11 +82,21 @@ class TreeNode:
         """
         return len(self.construction_log)
 
+    def __hash__(self) -> int:
+        return hash(self.fragments)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, TreeNode):
+            return False
+
+        return self.fragments == other.fragments
+
 
 class TreeSearcher:
     """A class that runs a tree search to generate high scoring molecules."""
 
     def __init__(self,
+                 fragment_to_index: dict[str, int],
                  reagent_to_fragments: dict[str, list[str]],
                  max_reactions: int,
                  scoring_fn: Callable[[str], float],
@@ -88,6 +105,7 @@ class TreeSearcher:
                  num_expand_nodes: int) -> None:
         """Creates the TreeSearcher object.
 
+        :param fragment_to_index: A dictionary mapping fragment SMILES to their index in the fragment file.
         :param reagent_to_fragments: A dictionary mapping a reagent SMARTS to all fragment SMILES that match that reagent.
         :param max_reactions: The maximum number of reactions to use to construct a molecule.
         :param scoring_fn: A function that takes as input a SMILES representing a molecule and returns a score.
@@ -95,9 +113,10 @@ class TreeSearcher:
         :param c_puct: The hyperparameter that encourages exploration.
         :param num_expand_nodes: The number of tree nodes to expand when extending the child nodes in the search tree.
         """
+        self.fragment_to_index = fragment_to_index
         self.reagent_to_fragments = reagent_to_fragments
-        self.all_fragment_tuples = sorted(
-            (fragment,) for fragments in self.reagent_to_fragments.values() for fragment in fragments
+        self.all_fragments = sorted(
+            fragment for fragments in self.reagent_to_fragments.values() for fragment in fragments
         )
         self.max_reactions = max_reactions
         self.scoring_fn = scoring_fn
@@ -108,7 +127,7 @@ class TreeSearcher:
 
         self.TreeNodeClass = partial(TreeNode, c_puct=c_puct, scoring_fn=scoring_fn)
         self.root = self.TreeNodeClass()
-        self.state_map: dict[tuple[str, ...], TreeNode] = {tuple(): self.root}
+        self.state_map: dict[TreeNode, TreeNode] = {self.root: self.root}
 
     # TODO: documentation
     @classmethod
@@ -141,7 +160,7 @@ class TreeSearcher:
             # Loop through products of reagent indices that the mols match to
             # and for each product, if it matches to all separate reagents,
             # then include the missing reagents in the set of unfilled reagents
-            for matched_reagent_indices in product(*reagent_matches_per_mol):
+            for matched_reagent_indices in itertools.product(*reagent_matches_per_mol):
                 matched_reagent_indices = set(matched_reagent_indices)
 
                 if len(matched_reagent_indices) == len(mols):
@@ -161,14 +180,21 @@ class TreeSearcher:
         return available_fragments
 
     # TODO: documentation
-    def get_next_fragment_tuples(self, fragments: tuple[str]) -> list[tuple[str, ...]]:
-        if len(fragments) == 0:
-            return self.all_fragment_tuples
+    def get_all_next_fragment_nodes(self, node: TreeNode) -> list[TreeNode]:
+        if len(node.fragments) == 0:
+            next_fragments = self.all_fragments
+        else:
+            next_fragments = self.get_next_fragments(fragments=node.fragments)
 
-        next_fragments = self.get_next_fragments(fragments=fragments)
-        next_fragment_tuples = [(*fragments, next_fragment) for next_fragment in next_fragments]
+        new_nodes = [
+            self.TreeNodeClass(
+                fragments=(*node.fragments, next_fragment),
+                construction_log=node.construction_log
+            )
+            for next_fragment in next_fragments
+        ]
 
-        return next_fragment_tuples
+        return new_nodes
 
     def get_reactions_for_fragments(self, fragments: tuple[str]) -> list[tuple[Reaction, dict[str, int]]]:
         mols = [convert_to_mol(fragment, add_hs=True) for fragment in fragments]
@@ -182,102 +208,108 @@ class TreeSearcher:
             reagent_matches_per_mol = self.get_reagent_matches_per_mol(reaction=reaction, mols=mols)
 
             # Include every assignment of fragments to reagents that fills all the reagents
-            for matched_reagent_indices in product(*reagent_matches_per_mol):
+            for matched_reagent_indices in itertools.product(*reagent_matches_per_mol):
                 if len(set(matched_reagent_indices)) == reaction.num_reagents:
                     fragment_to_reagent_index = dict(zip(fragments, matched_reagent_indices))
                     matching_reactions.append((reaction, fragment_to_reagent_index))
 
         return matching_reactions
 
-    def run_all_reactions_for_fragments(self, fragments: tuple[str]) -> list[str]:
-        matching_reactions = self.get_reactions_for_fragments(fragments=fragments)
+    def run_all_reactions(self, node: TreeNode) -> list[TreeNode]:
+        matching_reactions = self.get_reactions_for_fragments(fragments=node.fragments)
 
-        products = []
+        product_nodes = []
         for reaction, fragment_to_reagent_index in matching_reactions:
             # Put fragments in the right order for the reaction
-            sorted_fragments = sorted(fragments, key=lambda frag: fragment_to_reagent_index[frag])
+            fragments = sorted(node.fragments, key=lambda frag: fragment_to_reagent_index[frag])
 
             # Run reaction
-            reaction_products = reaction.run_reactants(sorted_fragments)
+            products = reaction.run_reactants(fragments)
 
-            if len(reaction_products) == 0:
+            if len(products) == 0:
                 raise ValueError('Reaction failed to produce products.')
 
-            assert all(len(product) == 1 for product in reaction_products)
+            assert all(len(product) == 1 for product in products)
 
             # Convert product mols to SMILES, remove Hs, and deduplicate (preserving order for random reproducibility)
-            products += list(dict.fromkeys(Chem.MolToSmiles(Chem.RemoveHs(product[0])) for product in reaction_products))
+            products = list(dict.fromkeys(Chem.MolToSmiles(Chem.RemoveHs(product[0])) for product in products))
 
-            # TODO: reaction log
-            # # Create reaction log
-            # reaction_log = {
-            #     'reaction_id': reaction.id,
-            #     'reagent_ids': [fragment_to_index.get(fragment, -1) for fragment in fragments],
-            #     'reagent_smiles': [fragment for fragment in fragments]
-            # }
+            # Create reaction log
+            reaction_log = {
+                'reaction_id': reaction.id,
+                'reagent_ids': [self.fragment_to_index.get(fragment, -1) for fragment in fragments],
+                'reagent_smiles': [fragment for fragment in fragments]
+            }
 
-        return products
+            product_nodes += [
+                self.TreeNodeClass(
+                    fragments=(product,),
+                    construction_log=node.construction_log + (reaction_log,)
+                )
+                for product in products
+            ]
 
-    def rollout(self, tree_node: TreeNode) -> float:
+        return product_nodes
+
+    def expand_node(self, node: TreeNode) -> list[TreeNode]:
+        # Run all valid reactions on the current fragments to generate new molecules
+        new_nodes = self.run_all_reactions(node=node)
+
+        # Add all possible next fragments to the current fragment
+        new_nodes += self.get_all_next_fragment_nodes(node=node)
+
+        return new_nodes
+
+    def rollout(self, node: TreeNode) -> float:
         """Performs a Monte Carlo Tree Search rollout.
 
-        :param tree_node: An TreeNode representing the root of the MCTS search.
+        :param node: An TreeNode representing the root of the MCTS search.
         :return: The value (reward) of the rollout.
         """
         # Stop the search if we've reached the maximum number of reactions
-        if tree_node.num_reactions >= self.max_reactions:
-            return tree_node.P
+        if node.num_reactions >= self.max_reactions:
+            return node.P
 
         # Expand if this node has never been visited
-        if len(tree_node.children) == 0:
-            # Run all valid reactions on the current fragments to generate new molecules
-            new_fragment_tuples = [
-                (product,) for product in self.run_all_reactions_for_fragments(fragments=tree_node.fragments)
-            ]
+        if len(node.children) == 0:
+            # Expand the node both by running reactions with the current fragments and adding new fragments
+            new_nodes = self.expand_node(node=node)
 
-            # Add all possible next fragments to the current fragments
-            new_fragment_tuples += self.get_next_fragment_tuples(fragments=tree_node.fragments)
-
-            if len(new_fragment_tuples) == 0:
+            if len(new_nodes) == 0:
                 raise NotImplementedError  # TODO: handle this case of no valid next fragments
 
             # Limit the number of nodes to expand
-            if len(new_fragment_tuples) > self.num_expand_nodes:
-                new_fragment_tuples = self.rng.choice(new_fragment_tuples, size=self.num_expand_nodes, replace=False)
+            if len(new_nodes) > self.num_expand_nodes:
+                new_nodes = self.rng.choice(new_nodes, size=self.num_expand_nodes, replace=False)
 
             # Add the expanded nodes as children
-            child_fragment_tuples = set()
-            for new_fragment_tuple in new_fragment_tuples:
+            for new_node in new_nodes:
 
                 # Check whether this node was already added as a child
-                if new_fragment_tuple not in child_fragment_tuples:
+                if new_node not in node.children:
 
                     # Check the state map and merge with an existing node if available
-                    if new_fragment_tuple not in self.state_map:
-                        self.state_map[new_fragment_tuple] = self.TreeNodeClass(fragments=new_fragment_tuple)
-
-                    new_node = self.state_map[new_fragment_tuple]
+                    new_node = self.state_map.setdefault(new_node, new_node)
 
                     # Add the new node as a child of the tree node
-                    tree_node.children.append(new_node)
-                    child_fragment_tuples.add(new_fragment_tuple)
+                    node.children.add(new_node)
 
         # Select the best child node and unroll it
-        sum_count = sum(child.N for child in tree_node.children)
-        selected_node = max(tree_node.children, key=lambda x: x.Q() + x.U(n=sum_count))
-        v = self.rollout(tree_node=selected_node)
+        sum_count = sum(child.N for child in node.children)
+        selected_node = max(node.children, key=lambda x: x.Q() + x.U(n=sum_count))
+        v = self.rollout(node=selected_node)
         selected_node.W += v
         selected_node.N += 1
 
         return v
 
-    def search(self) -> List[TreeNode]:
+    def search(self) -> list[TreeNode]:
         """Runs the tree search.
 
         :return: A list of TreeNode objects representing text masks sorted from highest to lowest reward.
         """
         for _ in range(self.n_rollout):
-            self.rollout(tree_node=self.root)
+            self.rollout(node=self.root)
 
         # Get all the nodes representing fully constructed molecules
         nodes = [node for _, node in self.state_map.items() if node.num_fragments == 1]
@@ -292,6 +324,7 @@ class TreeSearchRunner:
     """A class that creates instances of TreeSearcher to search for high scoring molecules."""
 
     def __init__(self,
+                 fragment_to_index: dict[str, int],
                  reagent_to_fragments: dict[str, list[str]],
                  max_reactions: int,
                  scoring_fn: Callable[[str], float],
@@ -300,6 +333,7 @@ class TreeSearchRunner:
                  num_expand_nodes: int) -> None:
         """Creates the TreeSearchRunner object.
 
+        :param fragment_to_index: A dictionary mapping fragment SMILES to their index in the fragment file.
         :param reagent_to_fragments: A dictionary mapping a reagent SMARTS to all fragment SMILES that match that reagent.
         :param max_reactions: The maximum number of reactions to use to construct a molecule.
         :param scoring_fn: A function that takes as input a SMILES representing a molecule and returns a score.
@@ -307,6 +341,7 @@ class TreeSearchRunner:
         :param c_puct: The hyperparameter that encourages exploration.
         :param num_expand_nodes: The number of tree nodes to expand when extending the child nodes in the search tree.
         """
+        self.fragment_to_index = fragment_to_index
         self.reagent_to_fragments = reagent_to_fragments
         self.max_reactions = max_reactions
         self.scoring_fn = scoring_fn
@@ -314,12 +349,13 @@ class TreeSearchRunner:
         self.c_puct = c_puct
         self.num_expand_nodes = num_expand_nodes
 
-    def search(self) -> List[TreeNode]:
+    def search(self) -> list[TreeNode]:
         """Runs the tree search.
 
         :return: A list of TreeNode objects representing text masks sorted from highest to lowest reward.
         """
         return TreeSearcher(
+            fragment_to_index=self.fragment_to_index,
             reagent_to_fragments=self.reagent_to_fragments,
             max_reactions=self.max_reactions,
             scoring_fn=self.scoring_fn,
