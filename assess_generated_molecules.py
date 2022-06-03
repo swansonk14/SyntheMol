37759308785f94a4fec0aeeca38ce5a067b5b69f
Kyle Swanson <swansonk.14@gmail.com>
2022-06-03 13:27:32 +0100

    Adding diversity compared to train molecules

--- assess_generated_molecules.py
+++ assess_generated_molecules.py
@@ -17,6 +17,7 @@ class Args(Tap):
     data_path: Path  # Path to CSV file containing generated molecules.
     save_dir: Path  # Path to directory where plots and results will be saved.
     train_hits_path: Optional[Path] = None  # Path to CSV file containing hits from the training set for computing novelty.
+    min_score: Optional[float] = None  # If provided, only molecules with scores >= this threshold are assessed.
     smiles_column: str = 'smiles'  # The name of the column containing SMILES in data_path.
     train_hits_smiles_column: str = 'smiles'  # The name of the column containing SMILES in train_hits_path.
     score_column: str = 'score'  # The name of the column containing scores.
@@ -47,16 +48,23 @@ def compute_pairwise_tanimoto_distances(mols_1: list[Union[str, Chem.Mol]],
 
 
 # TODO: load this from chem_utils
-def compute_min_tanimoto_distances(smiles: list[str]) -> np.ndarray:
-    """Computes the minimum Tanimoto distance between each molecule and every other molecule.
+def compute_min_tanimoto_distances(mols: list[Union[str, Chem.Mol]],
+                                   reference_mols: Optional[list[Union[str, Chem.Mol]]] = None) -> np.ndarray:
+    """Computes the minimum Tanimoto distance between each molecule and a set of molecules.
+
+    If only mols is provided, computes the minimum distance between each molecule and every other molecule in the set.
+    If reference_mols is provided, computes the minimum distance between each molecule in mols to the molecules in
+    reference_mols.
 
     Note: Does NOT remove duplicate SMILES before computing pairwise distances.
 
-    :param smiles: A list of SMILES.
-    :return: The minimum Tanimoto distance between each molecule and every other molecule.
+    :param mols: A list of SMILES for the molecules whose distances should be computed.
+    :param reference_mols: A list of SMILES that serves as the reference set against which distances are computed.
+    :return: The minimum Tanimoto distance between each molecule and
+             every other molecule (either in mols or reference_mols).
     """
     # Compute pairwise Tanimoto distances
-    pairwise_tanimoto_distances = compute_pairwise_tanimoto_distances(mols_1=smiles)
+    pairwise_tanimoto_distances = compute_pairwise_tanimoto_distances(mols_1=mols, mols_2=reference_mols)
 
     # Compute average minimum Tanimoto distance between each molecule and the other molecules
     np.fill_diagonal(pairwise_tanimoto_distances, np.inf)
@@ -81,7 +89,15 @@ def assess_generated_molecules(args: Args) -> None:
     """Assess the quality and diversity of generated molecules."""
     # Load data
     data = pd.read_csv(args.data_path)
-    print(f'Number of molecules = {len(data):,}')
+
+    # Threshold molecules
+    if args.min_score is not None:
+        data = data[data[args.score_column] >= args.min_score]
+        data.to_csv(args.save_dir / 'molecules.csv', index=False)
+
+    # Count molecules
+    num_molecules = len(data)
+    print(f'Number of molecules = {num_molecules:,}')
 
     # Get SMILES and scores
     smiles = data[args.smiles_column]
@@ -101,36 +117,57 @@ def assess_generated_molecules(args: Args) -> None:
     plt.tight_layout()
     plt.savefig(args.save_dir / 'scores.pdf')
 
-    # Assess diversity
-    min_tanimoto_distances = compute_min_tanimoto_distances(smiles)
-    diversity_mean = np.mean(min_tanimoto_distances)
-    diversity_std = np.std(min_tanimoto_distances)
-    print(f'Diversity = {diversity_mean:.3f} +/- {diversity_std:.3f}')
+    # Assess diversity within generated molecules
+    generated_min_tanimoto_distances = compute_min_tanimoto_distances(smiles)
+    generated_diversity_mean = np.mean(generated_min_tanimoto_distances)
+    generated_diversity_std = np.std(generated_min_tanimoto_distances)
+    print(f'Generated diversity = {generated_diversity_mean:.3f} +/- {generated_diversity_std:.3f}')
 
-    # Plot diversity distribution
+    # Plot diversity distribution within generated molecules
     plt.clf()
-    plt.hist(min_tanimoto_distances, bins=100)
-    plt.xlabel('Minimum Tanimoto Distance')
+    plt.hist(generated_min_tanimoto_distances, bins=100)
+    plt.xlabel('Minimum Tanimoto Distance between Generated Molecules')
     plt.ylabel('Count')
-    plt.title('Minimum Tanimoto Distance Distribution')
+    plt.title('Generated Minimum Tanimoto Distance Distribution')
     plt.tight_layout()
-    plt.savefig(args.save_dir / 'diversity.pdf')
+    plt.savefig(args.save_dir / 'generated_diversity.pdf')
 
-    # Assess novelty
+    # Compare to train hits
     if args.train_hits_path is not None:
+        # Load train hits
         train_hits = pd.read_csv(args.train_hits_path)
-        train_hits_smiles = set(train_hits[args.train_hits_smiles_column])
+        train_hits_smiles = train_hits[args.train_hits_smiles_column]
+
+        # Assess diversity compared to train
+        train_min_tanimoto_distances = compute_min_tanimoto_distances(mols=smiles, reference_mols=train_hits_smiles)
+        train_diversity_mean = np.mean(train_min_tanimoto_distances)
+        train_diversity_std = np.std(train_min_tanimoto_distances)
+        print(f'Train diversity = {train_diversity_mean:.3f} +/- {train_diversity_std:.3f}')
+
+        # Plot diversity distribution compared to train
+        plt.clf()
+        plt.hist(train_min_tanimoto_distances, bins=100)
+        plt.xlabel('Minimum Tanimoto Distance from Generated to Train Molecules')
+        plt.ylabel('Count')
+        plt.title('Train Minimum Tanimoto Distance Distribution')
+        plt.tight_layout()
+        plt.savefig(args.save_dir / 'train_diversity.pdf')
+
+        # Assess novelty
         novelty = compute_novelty(smiles=smiles, reference_smiles=train_hits_smiles)
         print(f'Novelty = {novelty:.3f}')
     else:
-        novelty = None
+        train_diversity_mean = train_diversity_std = novelty = None
 
     # Save data
     evaluation = pd.DataFrame(data=[{
+        'num_molecules': num_molecules,
         'score_mean': score_mean,
         'score_std': score_std,
-        'diversity_mean': diversity_mean,
-        'diversity_std': diversity_std,
+        'generated_diversity_mean': generated_diversity_mean,
+        'generated_diversity_std': generated_diversity_std,
+        'train_diversity_mean': train_diversity_mean,
+        'train_diversity_std': train_diversity_std,
         'novelty': novelty
     }])
     evaluation.to_csv(args.save_dir / 'evaluation.csv', index=False)
