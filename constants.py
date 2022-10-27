bb322c03fe9c570aee9c31be42d1291d0414cbb6
Kyle Swanson <swansonk.14@gmail.com>
2022-10-27 11:21:27 -0700

    Adding product smiles to valid reactant combinations

--- constants.py
+++ constants.py
@@ -3,3 +3,4 @@ REAL_SPACE_SIZE = 31507987117  # As of August 30, 2022
 REAL_REACTION_COL = 'reaction'
 REAL_REAGENT_COLS = ['reagent1', 'reagent2', 'reagent3', 'reagent4']
 REAL_TYPE_COL = 'Type'
+REAL_SMILES_COL = 'smiles'
--- get_valid_real_space_reactions.py
+++ get_valid_real_space_reactions.py
@@ -1,4 +1,4 @@
-"""Gets the valid reactant combinations for reactions across the whole REAL space."""
+"""Gets the valid reactant and product combinations for reactions across the whole REAL space."""
 import pickle
 from functools import partial
 from multiprocessing import Pool
@@ -8,7 +8,12 @@ import pandas as pd
 from tap import Tap
 from tqdm import tqdm
 
-from constants import REAL_REACTION_COL, REAL_REAGENT_COLS, REAL_SPACE_SIZE
+from constants import (
+    REAL_REACTION_COL,
+    REAL_REAGENT_COLS,
+    REAL_SMILES_COL,
+    REAL_SPACE_SIZE
+)
 from real_reactions import REAL_REACTIONS
 
 
@@ -25,14 +30,16 @@ class Args(Tap):
         self.save_dir.mkdir(parents=True, exist_ok=True)
 
 
-def get_valid_real_space_reactant_combinations_for_file(path: Path, reactions: list[int]) -> tuple[dict[int, set[tuple[int, ...]]], int]:
-    """Gets the valid reactant combinations for reactions for a single REAL space file."""
+def get_valid_real_space_reactant_combinations_for_file(
+        path: Path, reactions: list[int]
+) -> tuple[dict[int, set[tuple[int, ..., str]]], int]:
+    """Gets the valid reactant and product combinations for reactions for a single REAL space file."""
     # Load REAL data file
-    data = pd.read_csv(path, sep='\t', usecols=[REAL_REACTION_COL, *REAL_REAGENT_COLS])
+    data = pd.read_csv(path, sep='\t', usecols=[REAL_SMILES_COL, REAL_REACTION_COL, *REAL_REAGENT_COLS])
     data_size = len(data)
 
     # Create dictionary of valid reactants
-    valid_reactants = {}
+    valid_reactant_product_combos = {}
 
     # Loop through reactions
     for reaction in reactions:
@@ -42,20 +49,23 @@ def get_valid_real_space_reactant_combinations_for_file(path: Path, reactions: l
         # Determine non-na reactant columns
         reactant_cols = [col for col in REAL_REAGENT_COLS if reaction_data[col].notna().any()]
 
-        # Get valid reactant combinations
-        valid_reactants[reaction] = set(reaction_data[reactant_cols].itertuples(index=False, name=None))
+        # Set up all cols to extract
+        cols = [*reactant_cols, REAL_SMILES_COL]
 
-    return valid_reactants, data_size
+        # Get valid reactant combinations
+        valid_reactant_product_combos[reaction] = set(reaction_data[cols].itertuples(index=False, name=None))
+
+    return valid_reactant_product_combos, data_size
 
 
 def get_valid_real_space_reactant_combinations(args: Args) -> None:
-    """Gets the valid reactant combinations for reactions across the whole REAL space."""
+    """Gets the valid reactant and product combinations for reactions across the whole REAL space."""
     # Get paths to data files
     data_paths = sorted(args.data_dir.rglob('*.cxsmiles.bz2'))
     print(f'Number of files = {len(data_paths):,}')
 
-    # Create combined valid reactants dictionary
-    all_valid_reactants = {}
+    # Create combined valid reactant/product dictionary
+    all_valid_reactant_product_combos = {}
 
     # Set up map function
     if args.parallel:
@@ -65,18 +75,18 @@ def get_valid_real_space_reactant_combinations(args: Args) -> None:
         pool = None
         map_fn = map
 
-    # Set up reactants for file function
+    # Set up reactant/product for file function
     reactants_for_file_fn = partial(get_valid_real_space_reactant_combinations_for_file, reactions=args.reactions)
 
     # Loop through all REAL space files
     with tqdm(total=REAL_SPACE_SIZE) as progress_bar:
-        for valid_reactants, data_size in map_fn(reactants_for_file_fn, data_paths):
-            # Merge valid reactants
-            for reaction, reactants in valid_reactants.items():
-                if reaction not in all_valid_reactants:
-                    all_valid_reactants[reaction] = reactants
+        for valid_reactant_product_combos, data_size in map_fn(reactants_for_file_fn, data_paths):
+            # Merge valid reactant/product combos
+            for reaction, reactant_product_combo in valid_reactant_product_combos.items():
+                if reaction not in all_valid_reactant_product_combos:
+                    all_valid_reactant_product_combos[reaction] = reactant_product_combo
                 else:
-                    all_valid_reactants[reaction].update(reactants)
+                    all_valid_reactant_product_combos[reaction].update(reactant_product_combo)
 
             # Update progress bar
             progress_bar.update(data_size)
@@ -85,13 +95,13 @@ def get_valid_real_space_reactant_combinations(args: Args) -> None:
         pool.close()
 
     # Print number of valid reactants for each reaction
-    for reaction, reactants in all_valid_reactants.items():
-        print(f'Number of valid reactants for reaction {reaction} = {len(reactants):,}')
+    for reaction, reactant_product_combo in all_valid_reactant_product_combos.items():
+        print(f'Number of valid reactant/product combinations for reaction {reaction} = {len(reactant_product_combo):,}')
 
     # Save valid reactants with one file per reaction
-    for reaction, reactants in all_valid_reactants.items():
+    for reaction, reactant_product_combo in all_valid_reactant_product_combos.items():
         with open(args.save_dir / f'reaction_{reaction}.pkl', 'wb') as f:
-            pickle.dump(reactants, f)
+            pickle.dump(reactant_product_combo, f)
 
 
 if __name__ == '__main__':
