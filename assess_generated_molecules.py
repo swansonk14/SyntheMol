fe76d6f1765ac5184a5070a87b464d74dc1c4c6d
Kyle Swanson <swansonk.14@gmail.com>
2023-01-11 21:10:39 -0500

    Refactoring assess generated molecules to make it modular and save figure data

--- assess_generated_molecules.py
+++ assess_generated_molecules.py
@@ -6,22 +6,16 @@ from typing import Optional
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem.QED import qed
 from tap import Tap
-from tqdm import tqdm
 
 from chem_utils.molecular_similarities import compute_top_similarities
 
 
 class Args(Tap):
     data_path: Path  # Path to CSV file containing generated molecules.
-    save_dir: Path  # Path to directory where plots and results will be saved.
-    train_path: Optional[Path] = None  # Path to CSV file containing the training set for computing novelty.
-    train_hits_path: Optional[Path] = None  # Path to CSV file containing hits from the training set for computing novelty.
-    min_score: Optional[float] = None  # If provided, only molecules with scores >= this threshold are assessed.
+    save_dir: Path  # Path to directory where plots will be saved.
+    train_hits_path: Path = None  # Path to CSV file containing hits from the training set for computing novelty.
     smiles_column: str = 'smiles'  # The name of the column containing SMILES in data_path.
-    train_smiles_column: str = 'smiles'  # The name of the column containing SMILES in train_path.
     train_hits_smiles_column: str = 'smiles'  # The name of the column containing SMILES in train_hits_path.
     score_column: str = 'score'  # The name of the column containing scores.
 
@@ -29,170 +23,138 @@ class Args(Tap):
         self.save_dir.mkdir(parents=True, exist_ok=True)
 
 
-# TODO: bring back model score if necessary
-SCORE_TYPES = ['score']  # , 'model_score']
-SIMILARITY_TYPES = ['tanimoto', 'tversky']
+def plot_scores(scores: list[float], save_dir: Path) -> None:
+    """Plot score distribution.
 
-
-def compute_novelty(smiles: list[str], reference_smiles: set[str]) -> float:
-    """Computes the novelty of a set of molecules compared to a reference set (i.e., the proportion of new molecules).
-
-    :param smiles: A list of new SMILES.
-    :param reference_smiles: A reference set of SMILES against which to compute novelty.
-    :return: The novelty (proportion of new molecules) of the set of molecules.
+    :param scores: A list of scores.
+    :param save_dir: The directory where the plot will be saved.
     """
-    novelty = float(np.mean([smile not in reference_smiles for smile in smiles]))
+    # Plot score distribution
+    plt.clf()
+    plt.hist(scores, bins=100)
+    plt.xlabel('Score')
+    plt.ylabel('Count')
+    plt.title('Score Distribution')
+    plt.savefig(save_dir / f'score.pdf', bbox_inches='tight')
 
-    return novelty
+    # Save score distribution
+    fig_data = pd.DataFrame({'score': scores})
+    fig_data.to_csv(save_dir / f'score.csv', index=False)
 
 
-def assess_generated_molecules(args: Args) -> None:
-    """Assess the quality and diversity of generated molecules."""
-    # Results dictionary
-    results = {}
+def plot_similarity(
+        smiles: list[str],
+        similarity_type: str,
+        save_dir: Path,
+        reference_smiles: Optional[list[str]] = None,
+        reference_name: Optional[str] = None
+) -> None:
+    """Plot similarity distribution within a list of SMILES or between that list and a reference list.
 
-    # Load data
-    data = pd.read_csv(args.data_path)
+    :param smiles: A list of SMILES.
+    :param similarity_type: The type of similarity.
+    :param save_dir: The directory where the plot will be saved.
+    :param reference_smiles: A list of reference SMILES to compare against.
+    :param reference_name: The name of the reference list of SMILES.
+    """
+    # Compute similarities
+    max_similarities = compute_top_similarities(
+        similarity_type=similarity_type,
+        mols=smiles,
+        reference_mols=reference_smiles
+    )
 
-    # Threshold molecules
-    if args.min_score is not None:
-        data = data[data[args.score_column] >= args.min_score]
-        data.to_csv(args.save_dir / 'molecules.csv', index=False)
+    # Get reference name
+    if reference_name is None:
+        reference_name = 'Internal'
 
-    # Count molecules
-    results['num_molecules'] = len(data)
-    print(f'Number of molecules = {results["num_molecules"]:,}')
+    # Get save name
+    save_name = save_dir / f'{reference_name.lower()}_{similarity_type}_similarity'
 
-    # Get SMILES
-    smiles = data[args.smiles_column]
+    # Plot diversity distribution
+    plt.clf()
+    plt.hist(max_similarities, bins=100)
+    plt.xlabel(f'Maximum {reference_name} {similarity_type.title()} Similarity')
+    plt.ylabel('Count')
+    plt.title(f'Maximum {reference_name} {similarity_type.title()} Similarity Distribution')
+    plt.savefig(f'{save_name}.pdf', bbox_inches='tight')
 
-    for score_type in SCORE_TYPES:
-        scores = data[score_type]
+    # Save diversity distribution
+    fig_data = pd.DataFrame({f'max_{similarity_type}_similarity': max_similarities})
+    fig_data.to_csv(f'{save_name}.csv', index=False)
 
-        # Assess score distribution
-        results['score_mean'] = np.mean(scores)
-        results['score_std'] = np.std(scores)
-        print(f'{score_type.title()} = {results["score_mean"]:.3f} +/- {results["score_std"]:.3f}')
 
-        # Plot score distribution
-        plt.clf()
-        plt.hist(scores, bins=100)
-        plt.xlabel(score_type.title())
-        plt.ylabel('Count')
-        plt.title('Score Distribution')
-        plt.savefig(args.save_dir / f'{score_type}.pdf', bbox_inches='tight')
+def plot_reference_similarity(
+        smiles: list[str],
+        reference_smiles: list[str],
+        reference_name: str,
+        similarity_type: str,
+        save_dir: Path
+) -> None:
+    """Plot similarity distribution between a list of SMILES and a reference list of SMILES.
 
-    # Assess diversity within generated molecules
-    for similarity_type in SIMILARITY_TYPES:
-        # Compute similarities within generated molecules
-        generated_max_similarities = compute_top_similarities(
-            similarity_type=similarity_type,
-            mols=data[args.smiles_column]
-        )
-        results[f'generated_diversity_{similarity_type}_mean'] = np.mean(generated_max_similarities)
-        results[f'generated_diversity_{similarity_type}_std'] = np.std(generated_max_similarities)
-        print(f'Generated diversity {similarity_type} = '
-              f'{results[f"generated_diversity_{similarity_type}_mean"]:.3f} +/- '
-              f'{results[f"generated_diversity_{similarity_type}_std"]:.3f}')
+    :param smiles: A list of SMILES.
+    :param reference_smiles: A list of reference SMILES to compare against.
+    :param reference_name: The name of the reference list of SMILES.
+    :param similarity_type: The type of similarity.
+    :param save_dir: The directory where the plot will be saved.
+    """
+    # Compute maximum similarity to reference SMILES
+    max_similarities = compute_top_similarities(
+        similarity_type=similarity_type,
+        mols=smiles,
+        reference_mols=reference_smiles
+    )
 
-        # Plot diversity distribution within generated molecules
-        plt.clf()
-        plt.hist(generated_max_similarities, bins=100)
-        plt.xlabel(f'Maximum {similarity_type.title()} Similarity between Generated Molecules')
-        plt.ylabel('Count')
-        plt.title(f'Generated Maximum {similarity_type.title()} Similarity Distribution')
-        plt.savefig(args.save_dir / f'generated_diversity_{similarity_type}.pdf', bbox_inches='tight')
+    # Get reference file name
+    reference_file_name = reference_name.lower().replace(' ', '_')
 
-    # Compare to train hits
-    if args.train_hits_path is not None:
-        # Load train hits
-        train_hits = pd.read_csv(args.train_hits_path)
-        train_hits_smiles = train_hits[args.train_hits_smiles_column]
+    # Plot diversity distribution compared to train
+    plt.clf()
+    plt.hist(max_similarities, bins=100)
+    plt.xlabel(f'Maximum {similarity_type.title()} Similarity from Generated to {reference_name}')
+    plt.ylabel('Count')
+    plt.title(f'{reference_name} Maximum {similarity_type.title()} Similarity Distribution')
+    plt.savefig(save_dir / f'{reference_file_name}_{similarity_type}_similarity.pdf', bbox_inches='tight')
 
-        # Assess diversity compared to train
-        for similarity_type in SIMILARITY_TYPES:
-            train_hits_max_similarities = compute_top_similarities(
-                similarity_type=similarity_type,
-                mols=smiles,
-                reference_mols=train_hits_smiles
-            )
-            results[f'train_hits_diversity_{similarity_type}_mean'] = np.mean(train_hits_max_similarities)
-            results[f'train_hits_diversity_{similarity_type}_std'] = np.std(train_hits_max_similarities)
-            print(f'Train diversity {similarity_type} = '
-                  f'{results[f"train_hits_diversity_{similarity_type}_mean"]:.3f} +/- '
-                  f'{results[f"train_hits_diversity_{similarity_type}_std"]:.3f}')
+    # Save similarity distribution
+    fig_data = pd.DataFrame({f'max_{similarity_type}_similarity': max_similarities})
+    fig_data.to_csv(save_dir / f'{reference_file_name}_{similarity_type}_similarity.csv', index=False)
 
-            # Plot diversity distribution compared to train
-            plt.clf()
-            plt.hist(train_hits_max_similarities, bins=100)
-            plt.xlabel(f'Maximum {similarity_type.title()} Similarity from Generated to Train Hits')
-            plt.ylabel('Count')
-            plt.title(f'Train Hits Maximum {similarity_type.title()} Similarity Distribution')
-            plt.savefig(args.save_dir / f'train_hits_diversity_{similarity_type}.pdf', bbox_inches='tight')
 
-            # Assess novelty
-            results['novelty'] = compute_novelty(smiles=smiles, reference_smiles=train_hits_smiles)
-            print(f'Novelty = {results["novelty"]:.3f}')
+def plot_reactions_counts(num_reactions: list[int], save_dir: Path) -> None:
+    """Plot the frequency of each number of reactions.
 
-    # Compare to train
-    if args.train_path is not None:
-        # Load train
-        train = pd.read_csv(args.train_path)
-        train_smiles = train[args.train_smiles_column]
-
-        for similarity_type in SIMILARITY_TYPES:
-            train_top_10_similarities = compute_top_similarities(
-                similarity_type=similarity_type,
-                mols=smiles,
-                reference_mols=train_smiles,
-                top_k=10
-            )
-
-            plt.clf()
-            plt.hist(train_top_10_similarities, bins=100)
-            plt.xlabel(f'Top 10 {similarity_type.title()} Similarity from Generated to Train')
-            plt.ylabel('Count')
-            plt.title(f'Train Top 10 {similarity_type.title()} Similarity Distribution')
-            plt.savefig(args.save_dir / f'train_diversity_{similarity_type}.pdf', bbox_inches='tight')
-
-    # Distribution of number of reactions
-    reaction_counts = Counter(data['num_reactions'])
+    :param num_reactions: A list of numbers of reactions per molecule.
+    :param save_dir: The directory where the plot will be saved.
+    """
+    # Get reaction counts
+    reaction_counts = Counter(num_reactions)
     min_reactions, max_reactions = min(reaction_counts), max(reaction_counts)
     reaction_nums = range(min_reactions, max_reactions + 1)
+    reaction_counts = [reaction_counts[num_reactions] for num_reactions in reaction_nums]
 
+    # Plot reaction counts
     plt.clf()
-    plt.bar(reaction_nums, [reaction_counts[num_reactions] for num_reactions in reaction_nums])
+    plt.bar(reaction_nums, reaction_counts)
     plt.xticks(reaction_nums)
     plt.xlabel('Number of Reactions')
     plt.ylabel('Count')
     plt.title('Number of Reactions')
-    plt.savefig(args.save_dir / 'reaction_numbers.pdf', bbox_inches='tight')
+    plt.savefig(save_dir / 'reaction_numbers.pdf', bbox_inches='tight')
 
-    # Usage of fragments (unique fragments per molecule)
-    reagent_columns = [column for column in data.columns if column.startswith('reagent_') and column.endswith('_id')]
-    reagent_data = data[reagent_columns]
-    fragment_counter = Counter(
-        reagent
-        for _, reagent_row in reagent_data.iterrows()
-        for reagent in {int(reagent) for reagent in reagent_row.dropna()}
-        if reagent != -1
-    )
+    # Save reaction counts
+    fig_data = pd.DataFrame({'reaction_nums': reaction_nums, 'reaction_counts': reaction_counts})
+    fig_data.to_csv(save_dir / 'reaction_numbers.csv', index=False)
 
-    fragments_with_counts = fragment_counter.most_common()
-    fragments, fragment_counts = zip(*fragments_with_counts)
 
-    plt.clf()
-    plt.scatter(np.arange(len(fragment_counts)), fragment_counts)
+def plot_reaction_usage(data: pd.DataFrame, save_dir: Path) -> None:
+    """Plot the frequency with which each reaction is used (unique reactions per molecule).
 
-    for i, (fragment, count) in enumerate(fragments_with_counts[:3]):
-        plt.annotate(str(fragment), (i, count))
-
-    plt.xlabel('Sorted Fragment Index')
-    plt.ylabel('Count (# molecules containing the fragment)')
-    plt.title('Fragment Counts')
-    plt.savefig(args.save_dir / 'fragment_counts.pdf', bbox_inches='tight')
-
-    # Usage of reactions (unique reactions per molecules)
+    :param data: DataFrame containing reaction usage.
+    :param save_dir: The directory where the plot will be saved.
+    """
+    # Get reaction usage
     reaction_columns = [column for column in data.columns if column.startswith('reaction_') and column.endswith('_id')]
     reaction_data = data[reaction_columns]
     reaction_counter = Counter(
@@ -205,28 +167,103 @@ def assess_generated_molecules(args: Args) -> None:
     reaction_counts = [reaction_counter[reaction] for reaction in reactions]
     xticks = np.arange(len(reaction_counts))
 
+    # Plot reaction usage
     plt.clf()
     plt.bar(xticks, reaction_counts)
     plt.xticks(ticks=xticks, labels=reactions, rotation=45)
     plt.xlabel('Reaction')
     plt.ylabel('Count (# molecules containing the reaction)')
     plt.title('Reaction Counts')
-    plt.savefig(args.save_dir / 'reaction_counts.pdf', bbox_inches='tight')
+    plt.savefig(save_dir / 'reaction_counts.pdf', bbox_inches='tight')
 
-    # Assess QED scores
-    mols = [Chem.MolFromSmiles(s) for s in tqdm(smiles, desc='SMILES to mol')]
-    qed_scores = [qed(mol) for mol in tqdm(mols, desc='QED')]
+    # Save reaction usage
+    fig_data = pd.DataFrame({'reaction': reactions, 'count': reaction_counts})
+    fig_data.to_csv(save_dir / 'reaction_counts.csv', index=False)
 
+
+def plot_fragment_usage(data: pd.DataFrame, save_dir: Path) -> None:
+    """Plot the frequency with which each fragment is used (unique fragments per molecule).
+
+    :param data: DataFrame containing fragment usage.
+    :param save_dir: The directory where the plot will be saved.
+    """
+    # Get fragment usage
+    reagent_columns = [column for column in data.columns if column.startswith('reagent_') and column.endswith('_id')]
+    reagent_data = data[reagent_columns]
+    fragment_counter = Counter(
+        reagent
+        for _, reagent_row in reagent_data.iterrows()
+        for reagent in {int(reagent) for reagent in reagent_row.dropna()}
+        if reagent != -1
+    )
+
+    fragments_with_counts = fragment_counter.most_common()
+    fragments, fragment_counts = zip(*fragments_with_counts)
+
+    # Plot fragment usage
     plt.clf()
-    plt.hist(qed_scores, bins=100)
-    plt.xlabel('QED Score')
-    plt.ylabel('Count')
-    plt.title('QED Score Counts')
-    plt.savefig(args.save_dir / 'qed_scores.pdf', bbox_inches='tight')
+    plt.scatter(np.arange(len(fragment_counts)), fragment_counts)
 
-    # Save data
-    evaluation = pd.DataFrame(data=[results])
-    evaluation.to_csv(args.save_dir / 'evaluation.csv', index=False)
+    plt.xlabel('Sorted Fragment Index')
+    plt.ylabel('Count (# molecules containing the fragment)')
+    plt.title('Fragment Counts')
+    plt.savefig(save_dir / 'fragment_counts.pdf', bbox_inches='tight')
+
+    # Save fragment usage
+    fig_data = pd.DataFrame({'fragment': fragments, 'count': fragment_counts})
+    fig_data.to_csv(save_dir / 'fragment_counts.csv', index=False)
+
+
+def assess_generated_molecules(args: Args) -> None:
+    """Assess the quality and diversity of generated molecules."""
+    # Load generated molecules
+    data = pd.read_csv(args.data_path)
+
+    # Load train hits
+    train_hits = pd.read_csv(args.train_hits_path)
+
+    # Count molecules
+    print(f'Number of molecules = {len(data):,}')
+
+    # Score distribution
+    plot_scores(
+        scores=data['score'],
+        save_dir=args.save_dir
+    )
+
+    # Similarity within generated molecules
+    plot_similarity(
+        smiles=data[args.smiles_column],
+        similarity_type='tanimoto',
+        save_dir=args.save_dir
+    )
+
+    # Similarity between generated molecules and train hits
+    plot_similarity(
+        smiles=data[args.smiles_column],
+        similarity_type='tversky',
+        save_dir=args.save_dir,
+        reference_smiles=train_hits[args.train_hits_smiles_column],
+        reference_name='Train Hits'
+    )
+
+    # Number of reactions
+    plot_reactions_counts(
+        num_reactions=data['num_reactions'],
+        save_dir=args.save_dir
+    )
+
+    # Usage of reactions
+    plot_reaction_usage(
+        data=data,
+        save_dir=args.save_dir
+    )
+
+    # Usage of fragments
+    plot_fragment_usage(
+        data=data,
+        save_dir=args.save_dir
+    )
 
 
 if __name__ == '__main__':
