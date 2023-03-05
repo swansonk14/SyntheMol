fe61433337cf8abb8e749f40316151b3e3576133
Kyle Swanson <swansonk.14@gmail.com>
2023-03-04 16:13:20 -0800

    Beginning to rename and reorganize repo into package

---
+++ scripts/README.md
@@ -0,0 +1,7 @@
+# SyntheMol Scripts
+
+This directory contains scripts for running SyntheMol.
+
+TODO: list scripts
+
+Scripts for generating plots to analyze the data and results are in the `plots` directory.
---
+++ setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+# Load version number
+__version__ = ''
+
+version_file = Path(__file__).parent.absolute() / 'SyntheMol' / '_version.py'
+
+with open(version_file) as fd:
+    exec(fd.read())
+
+# Load README
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='SyntheMol',
+    version=__version__,
+    author='Kyle Swanson',
+    author_email='swansonk.14@gmail.com',
+    description='SyntheMol',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/swansonk14/SyntheMol',
+    download_url=f'https://github.com/swansonk14/SyntheMol/archive/refs/tags/v_{__version__}.tar.gz',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'SyntheMol': ['py.typed']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        "Typing :: Typed"
+    ],
+    keywords=[
+        'machine learning',
+        'drug design',
+        'generative models',
+        'synthesizable molecules'
+    ]
+)
