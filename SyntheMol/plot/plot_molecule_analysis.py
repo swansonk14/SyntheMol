b9dadf640083067f4d72243fa2e0c1df811c9d0a
Kyle Swanson <swansonk.14@gmail.com>
2023-03-20 22:10:41 -0700

    Plot import fix

--- SyntheMol/plot/plot_molecule_analysis.py
+++ SyntheMol/plot/plot_molecule_analysis.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 from SyntheMol.constants import SMILES_COL
-from plot_generated_molecule_analysis import plot_scores, plot_internal_similarity
+from SyntheMol.plot.plot_generated_molecule_analysis import plot_scores, plot_internal_similarity
 
 
 def plot_molecule_analysis(
