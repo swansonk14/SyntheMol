c67056d51c3da76df90a830928b0ef63f7065cdf
Kyle Swanson <swansonk.14@gmail.com>
2023-09-05 16:29:23 -0700

    Adding back average similarities calculation and seeding maximal independent set

--- scripts/analysis/analyze_score_vs_similarity.py
+++ scripts/analysis/analyze_score_vs_similarity.py
@@ -10,6 +10,19 @@ from tqdm import tqdm
 from synthemol.constants import SCORE_COL, SMILES_COL
 
 
+def compute_average_maximum_similarity(similarities: np.ndarray) -> float:
+    """Computes the average maximum similarity between a set of molecules.
+
+    :param similarities: A 2D numpy array of pairwise molecule similarities.
+    :return: Average maximum similarity.
+    """
+    np.fill_diagonal(similarities, -np.inf)
+    max_similarities = np.max(similarities, axis=1)
+    average_max_similarity = float(np.mean(max_similarities))
+
+    return average_max_similarity
+
+
 def compute_approximate_maximum_independent_set(
     similarities: np.ndarray, threshold: float, num_tries: int = 10
 ) -> int:
@@ -29,8 +42,8 @@ def compute_approximate_maximum_independent_set(
 
     # Compute approximate maximum independent set
     max_independent_set_size = 0
-    for _ in range(num_tries):
-        independent_set = nx.maximal_independent_set(similarities_graph)
+    for seed in range(num_tries):
+        independent_set = nx.maximal_independent_set(similarities_graph, seed=seed)
         max_independent_set_size = max(max_independent_set_size, len(independent_set))
 
     return max_independent_set_size
@@ -74,9 +87,11 @@ def analyze_score_vs_similarity(
                     "score_threshold": score_threshold,
                     "num_hits": 0,
                     "percent_hits": 0,
+                    "similarity": 0,
                     "max_independent_set": 0,
                     "num_hits_novel": 0,
                     "percent_hits_novel": 0,
+                    "similarity_novel": 0,
                     "max_independent_set_novel": 0,
                 }
             )
@@ -90,6 +105,9 @@ def analyze_score_vs_similarity(
             "score_threshold": score_threshold,
             "num_hits": len(hits),
             "percent_hits": len(hits) / len(data),
+            "similarity": compute_average_maximum_similarity(
+                similarities=pairwise_similarities
+            ),
             "max_independent_set": compute_approximate_maximum_independent_set(
                 similarities=pairwise_similarities, threshold=similarity_threshold
             ),
@@ -101,6 +119,7 @@ def analyze_score_vs_similarity(
         if len(hits_novel) == 0:
             result["num_hits_novel"] = 0
             result["percent_hits_novel"] = 0
+            result["similarity_novel"] = 0
             result["max_independent_set_novel"] = 0
             results.append(result)
             continue
@@ -112,6 +131,9 @@ def analyze_score_vs_similarity(
 
         result["num_hits_novel"] = len(hits_novel)
         result["percent_hits_novel"] = len(hits_novel) / len(data)
+        result["similarity_novel"] = compute_average_maximum_similarity(
+            similarities=pairwise_similarities_novel
+        )
         result[
             "max_independent_set_novel"
         ] = compute_approximate_maximum_independent_set(
