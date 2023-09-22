2ff3dbf16a2db029a3bf6cc375f6b6ce74d6f391
Kyle Swanson <swansonk.14@gmail.com>
2023-09-21 17:27:54 -0700

    Progress on WuXi phase 3 reactions

--- synthemol/reactions/wuxi.py
+++ synthemol/reactions/wuxi.py
@@ -82,14 +82,323 @@ WUXI_REACTIONS_PHASE_2 = (
 )
 
 WUXI_REACTIONS_PHASE_3 = (
+    # BR_COOH/COOMe/COOEt + BT + AF/AG/AK/AN
     Reaction(
         reactants=[
-            QueryMol(''),
-            QueryMol(''),
-            QueryMol('')
+            QueryMol('[c:1]-[#35].[O:3]=[C:2]([OH,O-])[*:4]'),  # BR_COOH/COOMe/COOEt
+            QueryMol('[#6:5]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('[N&!H0;!$(N[C,S]=[O,S,N]):6]')  # AF/AG/AK/AN
+        ],
+        product=QueryMol('[#6:5]-[#6:1].[O:3]=[C:2]([N:6])[*:4]'),
+        reaction_id=8,
+        chemical_space='WuXi'
+    ),
+    # BR_N-Boc + BT + SA/SB/SC/SD/SE
+    Reaction(
+        reactants=[
+            QueryMol('[c:1]-[#35].CC(C)(C)OC(=O)[N:2]'),  # BR_N-Boc
+            QueryMol('[#6:3]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('[O:5]=[C:4]([OH,O-])[*:6]')  # SA/SB/SC/SD/SE
+        ],
+        product=QueryMol('[#6:3]-[#6:1].[O:5]=[C:4]([N:2])[*:6]'),
+        reaction_id=9,
+        chemical_space='WuXi'
+    ),
+    # BR_N-Boc + BT + YV
+        Reaction(
+        reactants=[
+            QueryMol('[c:1]-[#35].CC(C)(C)OC(=O)[N:2]-[C:3]'),  # BR_N-Boc
+            QueryMol('[#6:4]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('[#6:5]-[#7]=C=O')  # YV
+        ],
+        product=QueryMol('[#6:4]-[#6:1].[#6:5]-[#7]-[#6](=O)-[#7:2]-[#6:3]'),
+        reaction_id=10,
+        chemical_space='WuXi'
+    ),
+    # BR_N-Boc + BT + LP
+        Reaction(
+        reactants=[
+            QueryMol('[c:1]-[#35].CC(C)(C)OC(=O)[N:2]-[C:3]'),  # BR_N-Boc
+            QueryMol('[#6:4]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('[#6:5]-[#17]')  # LP
+        ],
+        product=QueryMol('[#6:4]-[#6:1].[#6:3]-[#7:2]-[#6:5]'),
+        reaction_id=11,
+        chemical_space='WuXi'
+    ),
+    # BR_N-Boc + BT + HU
+        Reaction(
+        reactants=[
+            QueryMol('[c:1]-[#35].CC(C)(C)OC(=O)[N:2]'),  # BR_N-Boc
+            QueryMol('[#6:3]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('Cl[S:4]([*:6])(=[O:5])=[O:7]')  # HU
+        ],
+        product=QueryMol('[#6:3]-[#6:1].[N:2][S:4]([*:6])(=[O:5])=[O:7]'),
+        reaction_id=12,
+        chemical_space='WuXi'
+    ),
+    # BR_N-Boc + BT + QS
+        Reaction(
+        reactants=[
+            QueryMol('[c:1]-[#35].CC(C)(C)OC(=O)[N:2]-[C:3]'),  # BR_N-Boc
+            QueryMol('[#6:4]-[#5](-[#8])-[#8]'),  # BT
+            QueryMol('[#6:5]-[#6;D2:6]=O')  # QS
+        ],
+        product=QueryMol('[#6:4]-[#6:1].[#6:5]-[#6:6]-[#7:2]-[#6:3]'),
+        reaction_id=13,
+        chemical_space='WuXi'
+    ),
+    # COOH_N-Boc + AF/AG/AK/AN + YV
+        Reaction(
+        reactants=[
+            QueryMol('[O:2]=[C:1]([OH,O-])[*:3].CC(C)(C)OC(=O)[N:4]-[C:5]'),  # COOH_N-Boc
+            QueryMol('[N&!H0;!$(N[C,S]=[O,S,N]):6]'),  # AF/AG/AK/AN
+            QueryMol('[#6:7]-[#7]=C=O')  # YV
+        ],
+        product=QueryMol('[O:2]=[C:1]([N:6])[*:3].[#6:7]-[#7]-[#6](=O)-[#7:4]-[#6:5]'),
+        reaction_id=14,
+        chemical_space='WuXi'
+    ),
+    # COOH_N-Boc + AF/AG/AK/AN + LP
+        Reaction(
+        reactants=[
+            QueryMol('[O:2]=[C:1]([OH,O-])[*:3].CC(C)(C)OC(=O)[N:4]-[C:5]'),  # COOH_N-Boc
+            QueryMol('[N&!H0;!$(N[C,S]=[O,S,N]):6]'),  # AF/AG/AK/AN
+            QueryMol('[#6:7]-[#17]')  # LP
+        ],
+        product=QueryMol('[O:2]=[C:1]([N:6])[*:3].[#6:5]-[#7:4]-[#6:7]'),
+        reaction_id=15,
+        chemical_space='WuXi'
+    ),
+    # COOH_N-Boc + AF/AG/AK/AN + HU
+        Reaction(
+        reactants=[
+            QueryMol('[O:2]=[C:1]([OH,O-])[*:3].CC(C)(C)OC(=O)[N:4]'),  # COOH_N-Boc
+            QueryMol('[N&!H0;!$(N[C,S]=[O,S,N]):5]'),  # AF/AG/AK/AN
+            QueryMol('Cl[S:6]([*:8])(=[O:7])=[O:9]')  # HU
+        ],
+        product=QueryMol('[O:2]=[C:1]([N:5])[*:3].[N:4][S:6]([*:8])(=[O:7])=[O:9]'),
+        reaction_id=16,
+        chemical_space='WuXi'
+    ),
+    # COOH_N-Boc + AF/AG/AK/AN + QS
+        Reaction(
+        reactants=[
+            QueryMol('[O:2]=[C:1]([OH,O-])[*:3].CC(C)(C)OC(=O)[N:4]-[C:5]'),  # COOH_N-Boc
+            QueryMol('[N&!H0;!$(N[C,S]=[O,S,N]):6]'),  # AF/AG/AK/AN
+            QueryMol('[#6:7]-[#6;D2:8]=O')  # QS
+        ],
+        product=QueryMol('[O:2]=[C:1]([N:6])[*:3].[#6:7]-[#6:8]-[#7:4]-[#6:5]'),
+        reaction_id=17,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + YV + SA/SB/SC/SD/SE
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # YV
+            QueryMol('')  # SA/SB/SC/SD/SE
         ],
         product=QueryMol(''),
-        reaction_id=8,
+        reaction_id=18,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + YV + YV
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # YV
+            QueryMol('')  # YV
+        ],
+        product=QueryMol(''),
+        reaction_id=19,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + YV + LP
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # YV
+            QueryMol('')  # LP
+        ],
+        product=QueryMol(''),
+        reaction_id=20,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + YV + HU
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # YV
+            QueryMol('')  # HU
+        ],
+        product=QueryMol(''),
+        reaction_id=21,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + YV + QS
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # YV
+            QueryMol('')  # QS
+        ],
+        product=QueryMol(''),
+        reaction_id=22,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + LP + SA/SB/SC/SD/SE
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # LP
+            QueryMol('')  # SA/SB/SC/SD/SE
+        ],
+        product=QueryMol(''),
+        reaction_id=23,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + LP + YV
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # LP
+            QueryMol('')  # YV
+        ],
+        product=QueryMol(''),
+        reaction_id=24,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + LP + LP
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # LP
+            QueryMol('')  # LP
+        ],
+        product=QueryMol(''),
+        reaction_id=25,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + LP + HU
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # LP
+            QueryMol('')  # HU
+        ],
+        product=QueryMol(''),
+        reaction_id=26,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + LP + QS
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # LP
+            QueryMol('')  # QS
+        ],
+        product=QueryMol(''),
+        reaction_id=27,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + HU + SA/SB/SC/SD/SE
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # HU
+            QueryMol('')  # SA/SB/SC/SD/SE
+        ],
+        product=QueryMol(''),
+        reaction_id=28,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + HU + YV
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # HU
+            QueryMol('')  # YV
+        ],
+        product=QueryMol(''),
+        reaction_id=29,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + HU + LP
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # HU
+            QueryMol('')  # LP
+        ],
+        product=QueryMol(''),
+        reaction_id=30,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + HU + HU
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # HU
+            QueryMol('')  # HU
+        ],
+        product=QueryMol(''),
+        reaction_id=31,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + HU + QS
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # HU
+            QueryMol('')  # QS
+        ],
+        product=QueryMol(''),
+        reaction_id=32,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + SA/SB/SC/SD/SE + YV
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # SA/SB/SC/SD/SE
+            QueryMol('')  # YV
+        ],
+        product=QueryMol(''),
+        reaction_id=33,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + SA/SB/SC/SD/SE + LP
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # SA/SB/SC/SD/SE
+            QueryMol('')  # LP
+        ],
+        product=QueryMol(''),
+        reaction_id=34,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + SA/SB/SC/SD/SE + HU
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # SA/SB/SC/SD/SE
+            QueryMol('')  # HU
+        ],
+        product=QueryMol(''),
+        reaction_id=35,
+        chemical_space='WuXi'
+    ),
+    # NH_N-Boc + SA/SB/SC/SD/SE + QS
+        Reaction(
+        reactants=[
+            QueryMol(''),  # NH_N-Boc
+            QueryMol(''),  # SA/SB/SC/SD/SE
+            QueryMol('')  # QS
+        ],
+        product=QueryMol(''),
+        reaction_id=36,
         chemical_space='WuXi'
     ),
 )
