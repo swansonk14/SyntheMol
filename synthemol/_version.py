456228babde70a72eaaac99b99d0243f299fbbf2
Kyle Swanson <swansonk.14@gmail.com>
2024-03-21 23:23:13 -0700

    Increment version to 1.0.1

--- synthemol/_version.py
+++ synthemol/_version.py
@@ -1,6 +1,6 @@
 """Contains the version information for synthemol."""
 # major, minor, patch
-version_info = 1, 0, 0
+version_info = 1, 0, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
