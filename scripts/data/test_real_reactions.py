881fc7f8b329b5800720cbe340fbe8899e5c37e0
Kyle Swanson <swansonk.14@gmail.com>
2024-04-04 13:54:13 -0700

    Printing mismatching building blocks and reactants

--- scripts/data/test_real_reactions.py
+++ scripts/data/test_real_reactions.py
@@ -1,6 +1,7 @@
 """Test Enamine REAL reaction SMARTS for building blocks and products."""
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from synthemol.reactions import REAL_REACTIONS
@@ -16,12 +17,14 @@ def test_real_reactions(
     data_path: Path,
     building_blocks_path: Path,
     sample_num_per_reaction: int | None = None,
+    show_mismatches_num: int = 0,
 ) -> None:
     """Test Enamine REAL reaction SMARTS for building blocks and products.
 
     :param data_path: Path to a CSV file containing a sample of REAL data with building block IDs and product SMILES.
     :param building_blocks_path: Path to a CSV file containing building blocks with IDs and SMILES.
     :param sample_num_per_reaction: Number of rows to sample from the data for each reaction. If None, use all rows.
+    :param show_mismatches_num: Number of mismatches to show for each reactant and building block.
     """
     # Load data
     data = pd.read_csv(data_path)
@@ -62,16 +65,33 @@ def test_real_reactions(
 
             # For each reactant in the reaction, test all the building blocks
             for reactant_num, reactant in enumerate(reaction.reactants):
-                num_matches = sum(
-                    reactant.has_substruct_match(building_block)
-                    for building_block in reactant_building_blocks
+                # Determine which building blocks match the reactant
+                match_mask = np.array(
+                    [
+                        reactant.has_substruct_match(building_block)
+                        for building_block in reactant_building_blocks
+                    ]
                 )
-                percent_matches = 100 * num_matches / len(reactant_building_blocks)
+                percent_matches = 100 * np.sum(match_mask) / len(match_mask)
 
+                # Print statistics about the matches
                 print(
                     f"Reactant {reactant_num + 1}, building block {building_block_num + 1}: "
                     f"{percent_matches:.2f}% matches"
                 )
+
+                # Optionally show mismatching building blocks
+                if (
+                    show_mismatches_num > 0
+                    and building_block_num == reactant_num
+                    and not np.all(match_mask)
+                ):
+                    print("Reactant SMARTS")
+                    print(f"  {reactant.smarts}")
+                    print(f"Example mismatching building blocks:")
+                    mismatches = reactant_building_blocks[~match_mask]
+                    for mismatch in mismatches.head(show_mismatches_num):
+                        print(f"  {mismatch}")
             print()
         print()
 
