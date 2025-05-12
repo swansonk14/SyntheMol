b2ede24bb68f62a9f72d35482f5257d560b532ee
Kyle Swanson <swansonk.14@gmail.com>
2025-05-12 16:20:51 -0700

    Bumping version to 2.0.0 for SyntheMol-RL release

--- synthemol/_version.py
+++ synthemol/_version.py
@@ -1,6 +1,7 @@
 """Contains the version information for synthemol."""
+
 # major, minor, patch
-version_info = 1, 0, 4
+version_info = 2, 0, 0
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
